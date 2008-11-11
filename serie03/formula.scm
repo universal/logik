a23125f77eddcdc7e8b413c6e8f21c9af76867c4
Christopher Hlubek <hlubek@localhost.(none)>
2008-11-12 00:16:28 +0100

    Anfang Serie 3

---
+++ serie03/formula.scm
@@ -0,0 +1,80 @@
+#lang scheme
+
+(provide (all-defined-out))
+
+; Structures for constructing formulas.
+(define-struct variable (index)) 
+(define-struct negation (negated)) 
+(define-struct disjunction (left right)) 
+(define-struct conjunction (left right)) 
+(define-struct conditional (left right)) 
+(define-struct biconditional (left right)) 
+
+; Structure for a single assignment: variable |-> truth value. 
+(define-struct sass (index value)) 
+; Structure for an undefined value. 
+(define-struct undefined ()) 
+; Look-up of a variable value; an assignment is a list of sass. 
+(define (look-up index assignment) 
+  (if (null? assignment) 
+      (make-undefined) 
+      (if (= index (sass-index (first assignment))) 
+          (sass-value (first assignment)) 
+          (look-up index (rest assignment))))) 
+
+; Evaluate formula w.r.t. a variable assignment. 
+(define (evaluate formula assignment) 
+  (cond ((eq? formula #t) #t) 
+        ((eq? formula #f) #f) 
+        ((variable? formula) 
+         (look-up (variable-index formula) assignment)) 
+        ((negation? formula) 
+         (not (evaluate (negation-negated formula) assignment))) 
+        ((disjunction? formula) 
+         (or (evaluate (disjunction-left formula) assignment) 
+             (evaluate (disjunction-right formula) assignment))) 
+        ((conjunction? formula) 
+         (and (evaluate (conjunction-left formula) assignment) 
+              (evaluate (conjunction-right formula) assignment))) 
+        ((conditional? formula) 
+         (or (not (evaluate (conditional-left formula) assignment)) 
+             (evaluate (conditional-right formula) assignment))) 
+        ((biconditional? formula) 
+         (eq? (evaluate (biconditional-left formula) assignment) 
+              (evaluate (biconditional-right formula) assignment))))) 
+
+; Print formula 
+(define (formula->string formula) 
+  (cond ((eq? formula #t) "1") 
+        ((eq? formula #f) "0") 
+        ((variable? formula) (string-append "X_" (number->string (variable-index formula))))
+        ((negation? formula)
+         (string-append "-" (formula->string (negation-negated formula))))
+        ((disjunction? formula)
+         (string-append
+             "("
+             (formula->string (disjunction-left formula))
+             " \\/ "
+             (formula->string (disjunction-right formula))
+             ")")) 
+        ((conjunction? formula)
+         (string-append
+             "("
+             (formula->string (conjunction-left formula))
+             " /\\ "
+             (formula->string (conjunction-right formula))
+             ")")) 
+        ((conditional? formula) 
+         (string-append
+             "("
+             (formula->string (conditional-left formula))
+             " -> "
+             (formula->string (conditional-right formula))
+             ")")) 
+        ((biconditional? formula) 
+         (string-append
+             "("
+             (formula->string (biconditional-left formula))
+             " <-> "
+             (formula->string (biconditional-right formula))
+             ")"))))
---
+++ serie03/sudoku.scm
@@ -0,0 +1,45 @@
+#lang scheme
+
+(require "formula.scm")
+
+(define (sudoku-var i j k ) (+ (* i 100) (* j 10) k))
+
+(define (sudoku-neg-var i j k ) (* -1 (sudoku-var i j k)))
+
+(define N (list 1 2 3 4 5 6 7 8 9))
+
+(define (phi-matrix-1)
+  (for*/list ([i N]
+              [j N])
+    (for*/list ([k N])
+      (sudoku-var i j k))))
+
+(define (phi-matrix-2)
+  (for*/list ([i N]
+              [j N]
+              [k1 N]
+              [k2 N]
+              #:when (not (= k1 k2)))
+      (list (sudoku-neg-var i j k1) (sudoku-neg-var i j k2))))
+
+(define (sudoku-ref s i j)
+  (list-ref (list-ref s (- i 1)) (- j 1)))
+
+(define (phi-s s)
+  (for*/list ([i N]
+              [j N]
+              [k N]
+              #:when (= (sudoku-ref s i j) k))
+      (list (sudoku-var i j k))))
+
+(define sudoku-instance (list
+  (list 0 0 0  0 0 0  0 1 0)
+  (list 4 0 0  0 0 0  0 0 0)
+  (list 0 2 0  0 0 0  0 0 0)
+  (list 0 0 0  0 5 0  4 0 7)
+  (list 0 0 8  0 0 0  3 0 0)
+  (list 0 0 1  0 9 0  0 0 0)
+  (list 3 0 0  4 0 0  2 0 0)
+  (list 0 5 0  1 0 0  0 0 0)
+  (list 0 0 0  8 0 6  0 0 0)
+))
